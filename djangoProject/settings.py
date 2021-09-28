9405f7d965e6322063164caf53ee62462e6bea16
Jamie Maniulit <jmaniulit@csumb.edu>
2021-09-28 03:02:14 -0700

    register page

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'wishlistapp.apps.WishlistappConfig',
+    
 ]
 
 MIDDLEWARE = [
--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -18,6 +18,7 @@ from django.urls import path
 from django.conf.urls import include
 from wishlistapp import views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("<int:userID>", views.index, name="index"),
