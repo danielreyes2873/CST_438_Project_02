092957ebbfcbb21efaa69182f5ee46dadae3cf21
Nicky <43733664+Hathoom@users.noreply.github.com>
2021-10-05 08:43:19 -0700

    Passing Data through Render
    Figured out how to pass data through render on the list page.

--- list/views.py
+++ list/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from database.models import Users, Items
 
 # Create your views here.
 def index(request):
-    return render(request,"list/list.html")
+    firstUser = Users.objects.get(userID=1)
+    username = firstUser.username
+    return render(request,"list/list.html", {"username":username})
