404ed55620ef8de5f0e54a047274be643676b1c0
Nicky <43733664+Hathoom@users.noreply.github.com>
2021-10-09 16:24:29 -0700

    Deleting unnecessary files
    Deleting files I made that are not necessary

--- viewItem/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from database.models import Users, Items
-
-# Create your views here.
-def index(request, id):
-    item = Items.objects.get(ItemId=id)
-
-    return render(request, "viewItem/item.html", {"item":item})
