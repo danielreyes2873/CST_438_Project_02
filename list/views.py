9191db1e2d1b541ab16d3f4f71e1cdb18b96ea99
Nicky <43733664+Hathoom@users.noreply.github.com>
2021-10-11 19:30:28 -0700

    Update
    Update made due to update for DB

--- list/views.py
+++ list/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
-from database.models import Users, Items
+from database.models import Items
+from django.contrib.auth.models import User
 
 # Create your views here.
 def index(request):
-    users = Users.objects.all()
+    users = User.objects.all()
 
     items = Items.objects.all()
     return render(request,"list/list.html", {"items":items})
