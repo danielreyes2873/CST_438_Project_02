2e6a196172990deeab85456735ee37bd482820af
Daniel Reyes <daniereyes@csumb.edu>
2021-10-12 11:40:33 -0700

    api is up and running. Need capability to add/delete items among other functionality.

---
+++ apiapp/serializers.py
@@ -0,0 +1,10 @@
+# serializers.py
+from rest_framework import serializers
+
+from .models import Item
+
+
+class ItemSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Item
+        fields = ('name', 'url')
