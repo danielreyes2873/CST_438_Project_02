71d6948ea0df639892605162ad1c0e3d458417aa
Daniel Reyes <daniereyes@csumb.edu>
2021-10-19 16:20:21 -0700

    removed User table we defined. added more fields to the item table for users to save items to their wishlist.

--- database/models.py
+++ database/models.py
@@ -2,20 +2,17 @@ from django.db import models
 
 
 # Create your models here.
-class Users(models.Model):
-    userID = models.AutoField(primary_key=True)
-    username = models.CharField(max_length=50)
-    password = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.username
-
 
 class Items(models.Model):
     # users = models.ForeignKey(Users, on_delete=models.CASCADE)
     itemID = models.AutoField(primary_key=True)
     userId = models.IntegerField()
+    name = models.CharField(max_length=60, default="item")
     url = models.CharField(max_length=250)
+    imageUrl = models.CharField(max_length=250, default="image")
+    price = models.IntegerField(default=0)
+    quantity = models.IntegerField(default=0)
+    priority = models.IntegerField(default=0)
 
     def __str__(self):
         return self.url
