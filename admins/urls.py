697a7f7e28f797ec6b8a3df0bd88c8bc1c200738
Onyi A <89602534+Onyi21@users.noreply.github.com>
2021-10-22 06:05:02 -0700

    updating users feature works

--- admins/urls.py
+++ admins/urls.py
@@ -6,5 +6,6 @@ urlpatterns= [
     path('', views.index, name='index'),
     path('viewUsers', views.index2, name="index2"),
     path('createUserInfo', views.index3, name="index3"),
-    path('updaterUserInfo', views.index4, name="index4"),
+    path('updaterUserInfo', views.index5, name="index5"),
+    path('updatingUsers', views.index6, name="index6"),
 ]
--- admins/views.py
+++ admins/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 
 
@@ -24,5 +24,26 @@ def index3(request):
     return render(request, "admins/createUserInfo.html")
 
 
-def index4(request):
-    return render(request, "admins/updaterUserInfo.html")
+# def index4(request):
+#     return render(request, "admins/updaterUserInfo.html")
+
+def index5(request):
+    user = User.objects.all()
+
+    if request.method == "POST":
+        if request.POST.get('userID'):
+            u = User.objects.get(id=request.POST.get('userID'))
+            return render(request, "admins/updatingUsers.html", {"u":u})
+        if request.POST.get('ID'):
+            u = User.objects.get(id=request.POST.get('ID'))
+            if request.POST.get('username'):
+                u.username = request.POST.get('username')
+            if request.POST.get('password'):
+                u.set_password(request.POST.get('password'))
+            u.save()
+            return render(request, "admins/updaterUserInfo.html", {"users": user})
+
+    return render(request, "admins/updaterUserInfo.html", {"users": user})
+
+def index6(request):
+    return render(request, "updatingUsers.html")
