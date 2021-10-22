7fe66386e00f638cf6b03488fa702cd67fa76bea
Daniel Reyes <daniereyes@csumb.edu>
2021-10-22 01:34:23 -0700

    gave admin its own create user page that will redirect to the view users page upon successful user creation

--- admins/views.py
+++ admins/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 
 
 # Create your views here.
@@ -21,7 +23,14 @@ def index2(request):
 
 
 def index3(request):
-    return render(request, "admins/createUserInfo.html")
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("/admins/viewUsers")
+    else:
+        form = UserCreationForm()
+    return render(request, "admins/createUserInfo.html", {"form": form})
 
 
 def index4(request):
